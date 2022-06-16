5e59a7364d9bbc20ae15597270efd4655c929d1a
KelvinMYYZJ <Kelvin_MYYZJ@outlook.com>
2022-06-16 15:50:14 +0800

    improve user: it can be output

--- src/data/user.cpp
+++ src/data/user.cpp
@@ -7,6 +7,10 @@ User::User(const string& _user_name, const string& _name, const string& _passwor
            int _privilege)
     : username(_user_name), name(_name), password(_password), mail_addr(_mail_addr), privilege(_privilege) {}
 User& User::operator=(const User& other) = default;
+ostream& operator<<(ostream& out, const User& user) {
+  out << user.username << ' ' << user.name << ' ' << user.mail_addr << ' ' << user.privilege;
+  return out;
+}
 User::~User() = default;
 }  // namespace sjtu
 #endif  // TRAINTICKETSYSTEM_2022_SRC_DATA_USER_CPP
--- src/data/user.h
+++ src/data/user.h
@@ -12,8 +12,10 @@ class User {
   User();
   User(const string& _user_name, const string& _name, const string& _password, const string& _mail_addr,
        int _privilege);
+  friend ostream& operator<<(ostream& out, const User& user);
   User& operator=(const User& other);
   ~User();
 };
+ostream& operator<<(ostream& out, const User& user);
 };      // namespace sjtu
 #endif  // TRAINTICKETSYSTEM_2022_SRC_DATA_USER_H
