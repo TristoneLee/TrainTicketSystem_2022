cc69d8228752e2ffbd8537dbb73d2c9423de9277
KelvinMYYZJ <Kelvin_MYYZJ@outlook.com>
2022-06-17 13:02:40 +0800

    finish StationPasser

---
+++ src/data/station_pass.cpp
@@ -0,0 +1,10 @@
+#ifndef TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_CPP
+#define TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_CPP
+#include "station_pass.h"
+namespace sjtu {
+StationPass::StationPass() = default;
+StationPass::StationPass(const TrainID& _train_id, int _idx) : train_id(_train_id), idx(_idx) {}
+StationPass::StationPass(const StationPass& other) = default;
+StationPass::~StationPass() = default;
+}  // namespace sjtu
+#endif  // TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_CPP
---
+++ src/data/station_pass.h
@@ -0,0 +1,16 @@
+#ifndef TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_H
+#define TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_H
+#include "str.hpp"
+namespace sjtu {
+class StationPass {
+ public:
+  TrainID train_id;
+  int idx;  // index of station in train_id
+  StationPass();
+  StationPass(const TrainID& _train_id, int _idx);
+  StationPass(const StationPass& other);
+  ~StationPass();
+};
+
+}  // namespace sjtu
+#endif  // TRAINTICKETSYSTEM_2022_SRC_DATA_STATION_PASS_H
